cdd1d7bc58e2ac8d982e4d138a3f0291b5847014
dmitri minckevich <rauckees0811@gmail.com>
2024-02-16 20:50:42 +0300

    add 6 labs, class: Human and CollectionsDemo

---
+++ src/main/java/org/example/lab6/CollectionsDemo.java
@@ -0,0 +1,5 @@
+package org.example.lab6;
+
+public class CollectionsDemo {
+
+}
---
+++ src/main/java/org/example/lab6/Human.java
@@ -0,0 +1,65 @@
+package org.example.lab6;
+
+import java.util.Objects;
+
+public class Human {
+    private String secondName;
+    private String firstName;
+    private String surname;
+    private int age;
+
+    public Human(String secondName, String firstName, String surname, int age) {
+        this.secondName = secondName;
+        this.firstName = firstName;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Human human = (Human) o;
+        return age == human.age && Objects.equals(secondName, human.secondName)
+                && Objects.equals(firstName, human.firstName)
+                    && Objects.equals(surname, human.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(secondName, firstName, surname, age);
+    }
+    public
+}
